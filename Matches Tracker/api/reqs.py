584d2d8c52fcede53a64a6e490360952de2e1edf
Murilo <muriloaquinotrab@gmail.com>
2026-01-23 16:03:31 -0300

    Refactor API requests and add tournament functions

--- Matches Tracker/api/reqs.py
+++ Matches Tracker/api/reqs.py
@@ -11,7 +11,7 @@ TOKEN = os.getenv("TOKEN")
 if not TOKEN:
     raise ValueError("TOKEN não encontrado no arquivo .env")
 
-BASE_URL = "https://api.pandascore.co/csgo/matches"
+BASE_URL = "https://api.pandascore.co/csgo"
 
 def get_headers():
     return {
@@ -20,14 +20,33 @@ def get_headers():
     }
 
 def buscar_proximas_partidas():
-    response = requests.get(f"{BASE_URL}/upcoming", headers=get_headers())
+    response = requests.get(f"{BASE_URL}/matches/upcoming", headers=get_headers())
     return response.json() if response.status_code == 200 else []
 
 def buscar_partidas_ao_vivo():
-    response = requests.get(f"{BASE_URL}/running", headers=get_headers())
+    response = requests.get(f"{BASE_URL}/matches/running", headers=get_headers())
     return response.json() if response.status_code == 200 else []
 
 def buscar_partidas_passadas():
-    url = f"https://api.pandascore.co/csgo/matches/past?sort=-begin_at&per_page=10"
-    response = requests.get(url, headers=get_headers())
+    response = requests.get(f"{BASE_URL}/matches/past", headers=get_headers())
     return response.json() if response.status_code == 200 else []
+
+def torneios_cs():
+    response = requests.get(f"{BASE_URL}/tournaments", headers=get_headers())
+    return response.json() if response.status_code == 200 else []
+
+def buscar_torneio(tournament_id):
+    url = f"https://api.pandascore.co/tournaments/{tournament_id}"
+    
+    response = requests.get(url, headers=get_headers())
+    if response.status_code == 200:
+        return response.json()
+    return None
+
+def buscar_time(team_id):
+    # A rota da PandaScore para um time específico
+    url = f"https://api.pandascore.co/teams/{team_id}"
+    response = requests.get(url, headers=get_headers())
+    if response.status_code == 200:
+        return response.json()
+    return None
